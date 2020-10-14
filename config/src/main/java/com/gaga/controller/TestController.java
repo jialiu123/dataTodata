f5bc2c3aa909893c32824669057d13b33f27a636
jkuiop123 <1171464657@qq.com>
2020-10-14 15:19:28 +0800

    测试demo提交

---
+++ config/src/main/java/com/gaga/controller/TestController.java
@@ -0,0 +1,41 @@
+package com.gaga.controller;
+
+/**
+ * demo
+ *
+ * @author ：liujia
+ * @date ：Created in 2020/10/14 15:17
+ * @version: 1.0
+ */
+public class TestController {
+
+
+//    @Autowired
+//    private DimShscYsszService dimShscYsszService;
+//
+//    @GetMapping("/getEntity")
+//    @ResponseBody
+//    public Object getEntity(HttpServletRequest request) throws Exception {
+//
+//        return ResultUtils.success(dimShscYsszService.getOne(new LambdaQueryWrapper<DimShscYssz>()
+//                .eq(DimShscYssz::getGlyhmc, super.getUserFromSession(request).getUsername())
+//                .eq(DimShscYssz::getJlzt, 0)
+//        ));
+//    }
+//
+//    @PostMapping("/save")
+//    @ResponseBody
+//    public Object save(HttpServletRequest request, @RequestBody DimShscYssz entity) throws Exception {
+//
+//        entity.setGlyhmc(super.getUserFromSession(request).getUsername());
+//
+//        boolean flag = dimShscYsszService.saveOrUpdate(entity);
+//        if (flag) {
+//            return ResultUtils.success(entity);
+//        } else {
+//            return ResultUtils.error(null);
+//        }
+//    }
+
+
+}

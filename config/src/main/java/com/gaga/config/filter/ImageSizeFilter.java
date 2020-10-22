3cd79f88a58d38e80a7055d4da3f7ba76fcf8624
jkuiop123 <1171464657@qq.com>
2020-10-22 13:36:44 +0800

    图片裁剪方法

--- config/src/main/java/com/gaga/config/filter/ImageSizeFilter.java
+++ config/src/main/java/com/gaga/config/filter/ImageSizeFilter.java
@@ -1,7 +1,7 @@
 package com.gaga.config.filter;
 
 import cn.hutool.core.io.FileUtil;
-import com.winning.utils.ImageChangeSize;
+import com.gaga.utils.ImageChangeSize;
 import net.coobird.thumbnailator.Thumbnails;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
